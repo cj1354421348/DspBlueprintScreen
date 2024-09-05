2b4eed47304fc13dc3edaae4d7f7ae9b5935160f
广州pc <350977886@qq.com>
2024-09-05 22:22:29 +0800

    添加图片资源

---
+++ src/views/components/BuildingIcon.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="icon" :class="{'has-count': props.item.count !== undefined, 'selected': isSelected}" 
+    :title="props.item.alt" 
+    @click="toggleSelection">
+        <img :src="src" :alt="props.item.alt">
+        <div v-if="props.item.count !== undefined" class="count">{{props.item.count === 1 ? '' : props.item.count}}</div>
+        <div class="name">{{props.item.name}}</div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watchEffect } from 'vue';
+import { iconUrl } from '@/data/icons';
+
+const props = defineProps<{
+    item: {
+        id: number,
+        name: string,
+        alt?: string,
+        count?: number,
+    }
+}>();
+
+const src = ref('');
+const isSelected = ref(false);
+
+watchEffect(async () => {
+    src.value = '';
+    src.value = await iconUrl(props.item.id);
+});
+
+const toggleSelection = () => {
+    isSelected.value = !isSelected.value;
+};
+</script>
+
+<style lang="scss">
+.icon {
+    display: inline-block;
+    position: relative;
+    width: 3.8rem;
+    border: 2px solid transparent;
+    transition: border-color 0.3s;
+
+    &.has-count {
+        padding-bottom: .4em;
+    }
+
+    &.selected {
+        border-color: blue;
+    }
+
+    img {
+        width: 100%;
+        display: block;
+    }
+
+    .count {
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        font-size: 0.8em;
+        color: cyan;
+        text-shadow: 0 0 10px cyan;
+    }
+
+    .name {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        max-width: 100%;
+        font-size: 0.8em; /* 设置字体大小 */
+    }
+}
+</style>
