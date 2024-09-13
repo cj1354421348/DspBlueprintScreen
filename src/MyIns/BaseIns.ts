5e3168b516e229b5122ad61675f0cd5eb6775c15
广州pc <350977886@qq.com>
2024-09-13 23:09:44 +0800

    单例自动挂载到window上

--- src/MyIns/BaseIns.ts
+++ src/MyIns/BaseIns.ts
@@ -13,11 +13,19 @@
 //     return SingletonE;
 // }
 
-export class BaseIns{
-    static getInstance<T>(this: new () => T): T {
-        if(!(<any>this).instance){
-            (<any>this).instance = new this();
+export class BaseIns {
+    private static instance: BaseIns;
+
+    static getInstance<T extends BaseIns>(this: new () => T): T {
+        // 自动挂载子类到 window 对象
+        const className = this.name; // 获取当前类名
+        if (!(window as any)[className]) {
+            (window as any)[className] = this; // 将类挂载到 window
         }
-        return (<any>this).instance;
+        // 创建单例实例
+        if (!(this as any).instance) {
+            (this as any).instance = new this();  // 创建实例
+        }
+        return (this as any).instance;
     }
 }
